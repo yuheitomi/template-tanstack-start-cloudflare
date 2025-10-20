1c82256d1d91aff108fb89902efb68388e50ef70
Yuhei Kuratomi <yuhei.kuratomi@gmail.com>
2025-10-20 23:14:17 +0900

    Set up project with Cloudflare Workers integration, added Wrangler configuration, and updated dependencies. Introduced new scripts for deployment and type generation, and modified Vite configuration to include Cloudflare plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
+    cloudflare({ viteEnvironment: { name: "ssr"}}),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),

7c2c4c47992911c37debc6664f9911b4d08e5364
Yuhei Kuratomi <yuhei.kuratomi@gmail.com>
2025-12-04 10:12:06 +0900

    chore: update dependencies, enhance Vite and Vitest configurations, and add worker-configuration types for improved compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+    tsconfigPaths({
+      projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-  test: {
-    environment: 'jsdom',
-  },
-})
+});
 
-export default config
+export default config;
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,20 +4,20 @@ import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineWorkersConfig({
-  plugins: [tanstackStart(), viteReact(), tsconfigPaths()],
-  // resolve: {
-  //   alias: {
-  //     "@tanstack/react-start/server-entry": path.resolve(
-  //       process.cwd(),
-  //       "node_modules/@tanstack/react-start/dist/default-entry/esm/server.js",
-  //     ),
-  //   },
-  // },
+  plugins: [tsconfigPaths(), tanstackStart(), viteReact()],
   test: {
     poolOptions: {
       workers: {
         wrangler: { configPath: "./wrangler.jsonc" },
       },
     },
+    // deps: {
+    //   optimizer: {
+    //     ssr: {
+    //       enabled: true,
+    //       include: ["@tanstack/react-start", "@tanstack/router", "@tanstack/start-server-core"],
+    //     },
+    //   },
+    // },
   },
 });
